a7c72ae0dac9fc94577a1fc837ab04bfcae6592a
Charlton Trezevant <ct@ctis.me>
2019-02-15 01:13:22 -0500

    Added Homework 1 - Producer-consumer

---
+++ CGS3763/HW1/producer_consumer.c
@@ -0,0 +1,50 @@
+int num_iterations = 20;
+int busywait_amount = 0;
+
+int bufsize = 5;
+int buf[5];
+
+semaphore Full, Empty;
+
+void busy(int x){
+   for(; x > 0; x--);
+}
+
+void producer(){
+    int i, prod_index = 0;
+    for(i = 0; i < num_iterations; i++){
+            p(Empty);
+            cout << "Item " << i << " produced.\n";
+            buf[prod_index] = i;
+            prod_index = ++prod_index % bufsize;
+            v(Full);
+
+            if(busywait_amount > 0);
+               busy(busywait_amount);
+    }
+}
+
+void consumer(){
+    int i, cons_index = 0;
+    for(i = 0; i < num_iterations; i++){
+            p(Full);
+            cout << "Item " << buf[cons_index] << " consumed.\n";
+            buf[cons_index] = -1;
+            cons_index = ++cons_index % bufsize;
+            v(Empty);
+
+            if(busywait_amount > 0);
+               busy(busywait_amount);
+    }
+}
+
+main(){
+    initialsem(Full, 0);
+    initialsem(Empty, bufsize);
+
+    cobegin {
+        producer();
+        consumer();
+    }
+}
+
