a8a9b043407a3d87d1cbfc63bcf9865244437b17
Charlton Trezevant <ct@ctis.me>
2019-02-18 18:29:35 -0500

    Added final version of producer/consumer assignment

--- CGS3763/HW1/producer_consumer.c
+++ CGS3763/HW1/producer_consumer.c
@@ -1,10 +1,16 @@
+/*
+  Charlton Trezevant 
+  CGS 3763 - Euripides Montagne
+  Homework #1 Producer-Consumer
+ */
+
 int num_iterations = 20;
 int busywait_amount = 0;
 
 int bufsize = 5;
 int buf[5];
 
-semaphore Full, Empty;
+semaphore Full, Empty, Mutex;
 
 void busy(int x){
    for(; x > 0; x--);
@@ -14,9 +20,11 @@ void producer(){
     int i, prod_index = 0;
     for(i = 0; i < num_iterations; i++){
             p(Empty);
+            p(Mutex);
             cout << "Item " << i << " produced.\n";
             buf[prod_index] = i;
             prod_index = ++prod_index % bufsize;
+            v(Mutex);
             v(Full);
 
             if(busywait_amount > 0);
@@ -28,9 +36,11 @@ void consumer(){
     int i, cons_index = 0;
     for(i = 0; i < num_iterations; i++){
             p(Full);
+            p(Mutex);
             cout << "Item " << buf[cons_index] << " consumed.\n";
             buf[cons_index] = -1;
             cons_index = ++cons_index % bufsize;
+            v(Mutex);
             v(Empty);
 
             if(busywait_amount > 0);
@@ -41,10 +51,10 @@ void consumer(){
 main(){
     initialsem(Full, 0);
     initialsem(Empty, bufsize);
+    initialsem(Mutex, 1);
 
     cobegin {
         producer();
         consumer();
     }
 }
-
